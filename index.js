4866d4775f56961bef8cb804e5bb9c489b03b65a
Sheryl Chee <sherylmichaela@gmail.com>
2023-12-17 09:24:15 +1100

    complete lab

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increment(n) {
+  return n + 1;
+}
+increment(a);
+
+function decrement(n) {
+  return n - 1;
+}
+decrement(a);
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+makeInt("sdfdsf");
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+
+console.log(preserveDecimal("asdasdasd"));
